d89bef24f81cf221a47026e55d1222c0037a7d18
wh1tneyr <rioswhit@gmail.com>
2025-01-04 19:25:54 -0300

    analizando estadisticas descriptivas del dataset

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,3 +4,9 @@ sys.path.append(os.getcwd())
 
 #importar dataset 
 data = pd.read_csv('data/creditcard_2023.csv')
+
+#ver informacion del dataset
+data.info()
+
+#ver descripcion del df
+data.describe()
